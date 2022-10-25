e2cf0f3a4493c868530e6aada76747daf46d2b2a
alibabaEFC <13921068890@163.com>
2022-10-25 11:04:26 +0800

    first commit

---
+++ src/main/java/com/atguigu/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.atguigu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    public String portal(){
+
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "success";
+
+    }
+
+
+
+}
